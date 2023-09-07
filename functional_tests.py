d2dc743f7e41f80c89d94324fb2f01f6de430907
Vincent Wang <vincentdenghui@gmail.com>
2023-09-07 20:10:06 +1000

    Add functional tests

---
+++ functional_tests.py
@@ -0,0 +1,49 @@
+import requests
+import unittest
+
+
+
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        ...
+    def tearDown(self):
+        ...
+
+    def get_url(self,route):
+        return f'http://localhost:8000{route}'
+
+    # FRi1. can visit the home page
+    def test_can_visit_the_default_home_page(self):
+        response = requests.get(self.get_url('/'))
+        assert response.status_code == 200
+        assert 'django' in response.content.decode('utf-8')
+
+    # FR1. Create a new Lender
+
+    # FR2. List all Lenders (five per page)
+
+    # FR1. List active lenders
+
+    # FR3. Get a specific Lender
+
+    # FR4. Update a specific Lender
+
+    # FR5. Delete a specific Lender
+
+    # FR6. Bulk upload Lenders in CSV format
+
+    # FR7. Download Lenders in CSV format
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
