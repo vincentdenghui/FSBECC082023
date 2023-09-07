81c1a90f338d5f06419f57c98ddb424249ef7e83
Vincent Wang <vincentdenghui@gmail.com>
2023-09-07 21:11:57 +1000

    Add ViewSet for Lender with a customized CSV renderer and readonly permission for non-users

--- lenders/views.py
+++ lenders/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from lenders.models import Lender
+from lenders.serializers import LenderSerializer
+from rest_framework import viewsets, permissions
+from rest_framework.settings import api_settings
+from lenders.renderers import CSVRenderer
 
-# Create your views here.
+
+class LenderViewSet(viewsets.ModelViewSet):
+    queryset = Lender.objects.all()
+    serializer_class = LenderSerializer
+    renderer_classes = tuple(api_settings.DEFAULT_RENDERER_CLASSES) + (CSVRenderer,)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
