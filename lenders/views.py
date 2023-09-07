7b464cd3181c6ba2deae68950b5d24d075a4788f
Vincent Wang <vincentdenghui@gmail.com>
2023-09-07 23:47:49 +1000

    Add filter and ordering support to the SetView

--- lenders/views.py
+++ lenders/views.py
@@ -1,6 +1,8 @@
 from lenders.models import Lender
 from lenders.serializers import LenderSerializer
 from rest_framework import viewsets, permissions
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.settings import api_settings
 from lenders.renderers import CSVRenderer
 
@@ -9,5 +11,9 @@ class LenderViewSet(viewsets.ModelViewSet):
     serializer_class = LenderSerializer
     renderer_classes = tuple(api_settings.DEFAULT_RENDERER_CLASSES) + (CSVRenderer,)
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['active','code']
+    ordering_fields = ['created', 'code', 'upfront_commission_rate', 'trial_commission_rate','active']
 
     lookup_field = 'code'
