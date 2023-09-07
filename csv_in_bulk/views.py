55dc6ca7856cd16da6d1b93dc8bba3323901890d
Vincent Wang <vincentdenghui@gmail.com>
2023-09-07 22:26:01 +1000

    Add csv_in_bulk view placeholder function for routing

--- csv_in_bulk/views.py
+++ csv_in_bulk/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+@csrf_exempt
+@require_http_methods(['GET', 'POST'])
+def csv_in_bulk(request):
+    ...
