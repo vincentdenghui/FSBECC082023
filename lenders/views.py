22909159d12a68b48f378c8c9eed3c2d8d5715cc
Vincent Wang <vincentdenghui@gmail.com>
2023-09-07 22:52:45 +1000

    Fix the missing lookup field in the ViewSet

--- lenders/views.py
+++ lenders/views.py
@@ -4,9 +4,10 @@ from rest_framework import viewsets, permissions
 from rest_framework.settings import api_settings
 from lenders.renderers import CSVRenderer
 
-
 class LenderViewSet(viewsets.ModelViewSet):
     queryset = Lender.objects.all()
     serializer_class = LenderSerializer
     renderer_classes = tuple(api_settings.DEFAULT_RENDERER_CLASSES) + (CSVRenderer,)
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    lookup_field = 'code'
