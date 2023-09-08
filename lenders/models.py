e1632490f359629501c7be21f0a9dde5113f1502
Vincent Wang <vincentdenghui@gmail.com>
2023-09-08 12:05:13 +1000

    Fix typo in class LenderFieldConstrains->LenderFieldConstraints

--- lenders/models.py
+++ lenders/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator, RegexValidator
 
-class LenderFieldConstrains:
+class LenderFieldConstraints:
 
     code_format_regex = RegexValidator(r'^[A-Z]*$', 'only capital alphabets [A-Z] are allowed.')
 
@@ -20,21 +20,21 @@ class LenderFieldConstrains:
 class Lender(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(blank=False,
-                            max_length=LenderFieldConstrains.name_max_length,
+                            max_length=LenderFieldConstraints.name_max_length,
                             validators=[
-                                MinLengthValidator(LenderFieldConstrains.name_min_length)])
+                                MinLengthValidator(LenderFieldConstraints.name_min_length)])
 
     code = models.CharField(unique=True,
-                            max_length=LenderFieldConstrains.code_max_length,
+                            max_length=LenderFieldConstraints.code_max_length,
                             validators=[
-                                MinLengthValidator(LenderFieldConstrains.code_min_length),
-                                LenderFieldConstrains.code_format_regex,])
+                                MinLengthValidator(LenderFieldConstraints.code_min_length),
+                                LenderFieldConstraints.code_format_regex,])
 
-    upfront_commission_rate = models.FloatField(validators=[MinValueValidator(LenderFieldConstrains.upfront_commission_rate_min),
-                                                            MaxValueValidator(LenderFieldConstrains.upfront_commission_rate_max)])
+    upfront_commission_rate = models.FloatField(validators=[MinValueValidator(LenderFieldConstraints.upfront_commission_rate_min),
+                                                            MaxValueValidator(LenderFieldConstraints.upfront_commission_rate_max)])
 
-    trial_commission_rate = models.FloatField(validators=[MinValueValidator(LenderFieldConstrains.trial_commission_rate_min),
-                                                          MaxValueValidator(LenderFieldConstrains.trial_commission_rate_max)])
+    trial_commission_rate = models.FloatField(validators=[MinValueValidator(LenderFieldConstraints.trial_commission_rate_min),
+                                                          MaxValueValidator(LenderFieldConstraints.trial_commission_rate_max)])
     active = models.BooleanField(default=True)
 
     class Meta:
